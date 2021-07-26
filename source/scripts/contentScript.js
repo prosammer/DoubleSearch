4b5fee6bde1f6b460e792f3a4a71cb35a003ed16
Sam Finton <spamhinting@gmail.com>
2021-07-25 23:46:59 -0700

    Moved Button to ToolBar as Div.

--- source/scripts/contentScript.js
+++ source/scripts/contentScript.js
@@ -7,47 +7,36 @@ let s = document.getElementsByTagName('script')[0];
 s.parentNode.insertBefore(script, s);
 
 
-//    Create Sidebar Toggle Button - visible by default
-let btn = document.createElement("button");
+//    Create "Toggle Results" div
+let btn = document.createElement("div");
 btn.id = 'toggleButton';
 btn.innerHTML = "Toggle Results";
-btn.style.cssText = "background-color: #f5f5f5;border:none;color:#707070;font-size:15px;padding: 10px 20px;margin:0 35% 0 0;border-radius:4px;outline:none;";
+btn.style.paddingLeft = "163px";
 btn.addEventListener("click", toggleSidebar)
-let btnDiv = document.createElement('div');
-btnDiv.style.display = 'flex';
-btnDiv.style.justifyContent = 'flex-end';
-btnDiv.appendChild(btn);
 
-let slimAppBar = document.getElementById('before-appbar');
-slimAppBar.insertBefore(btnDiv,slimAppBar.childNodes[0]);
+let toolbar = document.getElementById('hdtb-msb');
+toolbar.appendChild(btn);
 
 
-
-//  Create Sidebar - invisible by default
+//  Create Sidebar
 let resultsDiv = document.createElement("div");
 resultsDiv.className = 'gcse-searchresults-only';
 
 let sidebar  = document.createElement('div');
 sidebar.id = 'sidebarContainer';
-
-
 sidebar.style.cssText = ' float:left;\n' +
     ' min-width:268px;\n' +
     ' display:block;\n' +
     ' position:relative;\n' +
     ' padding-bottom:15px;\n' +
     ' margin-left:892px;\n' +
-    ' padding-right:8px';
-
-
+    ' padding-right:10px';
 
 sidebar.appendChild(resultsDiv);
 
 
 let rhs = document.getElementById('rhs');
 let mainDiv = document.getElementById('rcnt');
-
-
 
 if (rhs) {
     console.log("RHS exists, inserting as display: none.");
@@ -57,7 +46,6 @@ if (rhs) {
     console.log("RHS doesn't exist, inserting as display:block");
     mainDiv.appendChild(sidebar);
 }
-
 
 
 function toggleSidebar() {
