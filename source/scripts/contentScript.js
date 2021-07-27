266f68fe4757422d5451ab48091ccb236690a258
Sam Finton <spamhinting@gmail.com>
2021-07-27 09:50:17 -0700

    Added jQuery to project and modified search engine to do all 3 sites — jQuery code not working properly and sidebar element disappearing

--- source/scripts/contentScript.js
+++ source/scripts/contentScript.js
@@ -1,26 +1,63 @@
 // Adds Script to DOM
+import './jquery-3.6.0.min'
 let script = document.createElement("script");
 script.type = 'text/javascript';
 script.async = true;
-script.src = "https://cse.google.com/cse.js?cx=183c9a6555cdce95e";
+script.src = "https://cse.google.com/cse.js?cx=20929241749f9dd83";
+
 let s = document.getElementsByTagName('script')[0];
 s.parentNode.insertBefore(script, s);
 
 
-//    Create "Toggle Results" div
-let btn = document.createElement("div");
-btn.id = 'toggleButton';
-btn.innerHTML = "Toggle Results";
-btn.style.paddingLeft = "163px";
-btn.addEventListener("click", toggleSidebar)
+let defaultSite = "https://reddit.com";
+// let selectedSite = defaultSite;
+
+
+//    Create Reddit results div
+let redditBtn = document.createElement("div");
+redditBtn.id = 'redditBtn';
+redditBtn.innerHTML = 'Reddit';
+redditBtn.style.paddingLeft = '163px';
+// redditBtn.addEventListener("click", toggleSidebar("https://reddit.com"));
+redditBtn.addEventListener("click", function() {
+    toggleSidebar("https://reddit.com");
+});
+
+//    Create SO results div
+let soBtn = document.createElement("div");
+soBtn.id = 'soBtn';
+soBtn.innerHTML = "StackOverflow";
+soBtn.style.paddingLeft = '12px';
+// soBtn.addEventListener("click", toggleSidebar("https://stackoverflow.com"));
+soBtn.addEventListener("click", function() {
+    toggleSidebar("https://stackoverflow.com");
+});
+
+//    Create SO results div
+let hnBtn = document.createElement("div");
+hnBtn.id = 'hnBtn';
+hnBtn.innerHTML = "HackerNews";
+hnBtn.style.paddingLeft = '12px';
+// hnBtn.addEventListener("click", toggleSidebar("https://news.ycombinator.com"));
+hnBtn.addEventListener("click", function() {
+    toggleSidebar("https://news.ycombinator.com");
+});
+
+
 
 let toolbar = document.getElementById('hdtb-msb');
-toolbar.appendChild(btn);
+toolbar.appendChild(redditBtn);
+toolbar.appendChild(soBtn);
+toolbar.appendChild(hnBtn);
+
+
 
 
 //  Create Sidebar
 let resultsDiv = document.createElement("div");
+resultsDiv.setAttribute('data-as_sitesearch', defaultSite);
 resultsDiv.className = 'gcse-searchresults-only';
+resultsDiv.id = 'resultDiv';
 
 let sidebar  = document.createElement('div');
 sidebar.id = 'sidebarContainer';
@@ -41,30 +78,24 @@ let mainDiv = document.getElementById('rcnt');
 
 
 if (rhs) {
-    console.log("RHS exists, inserting as display: none.");
     sidebar.style.display = 'none';
     mainDiv.insertBefore(sidebar,rhs);
 } else {
-    console.log("RHS doesn't exist, inserting as display:block");
-    // mainDiv.appendChild(sidebar);
     mainDiv.insertBefore(sidebar,mainDiv.childNodes[1]);
 }
 
 
-
-
-function toggleSidebar() {
-    console.log('Running toggleSidebar Function');
-
+function toggleSidebar(selectedSite) {
+    console.log(selectedSite);
+    // resultsDiv.setAttribute('data-as_sitesearch', selectedSite);
+    // resultsDiv.reload();
+    console.log("I think i loaded JQuery!");
     if (sidebar.style.display === "none") {
-        console.log("sidebar.style.display is none, switching");
         if(rhs) {
             rhs.style.display = 'none';
         }
         sidebar.style.display = "block";
-
     } else {
-        console.log("sidebar.style.display is block, switching");
         if(rhs) {
             rhs.style.display = 'block';
         }
--- source/scripts/options.js
+++ source/scripts/options.js
@@ -1,5 +1,5 @@
 import 'emoji-log';
 
 import '../styles/options.scss';
-
+import 'jquery'
 console.emoji('🦄', 'Hello World from options main file!');
